b31e2cd76f17dbbcf31d61eee323a6cf84958108
Tangaraja <48880366+Tangaraja@users.noreply.github.com>
2019-05-25 12:24:47 +0800

    Update tangy.sh

--- tangy.sh
+++ tangy.sh
@@ -1,7 +1,7 @@
-date
-sleep 10
-hostname
-uptime
-uname -a
-touch /tmp/test.txt
+date;
+sleep 10;
+hostname;
+uptime;
+uname -a;
+touch /tmp/test.txt;
 echo "Testing for the first time "  >> /tmp/test.txt
